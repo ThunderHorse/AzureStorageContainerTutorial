924fcfbc5591f0dac427fc88c6f1c39f75647a84
Pease, Jason <jason.pease@aprimo.com>
2016-11-22 16:38:29 -0500

    First Azure Storage tutorial commit

---
+++ AzureStorageTutorial/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure; //CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; //CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; //Blob storage types
+
+namespace AzureStorageTutorial
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Program p = new Program();
+			p.runProgram();
+		}
+
+		private void runProgram()
+		{
+			//CloudStorageAccount - Reference to cloud storage account application is using
+			var cloudStorageAccount = parseConnectionString();
+
+			//CloudBlobClient - Allows you to retrieve containers and blobs stored in Blob storage
+			var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+			//CloudBlobContainer - Reference to a container (every blob in Azure storage must reside in a container)
+			CloudBlobContainer container = cloudBlobClient.GetContainerReference("container");
+			container.CreateIfNotExists();
+
+			//Set permissions on cloudBlobContainer
+			setContainerPermissions(container);
+		}
+
+		//Parse the connection string and return a reference to the storage account
+		private CloudStorageAccount parseConnectionString()
+		{
+			CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(
+				CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+			return cloudStorageAccount;
+		}
+
+		//Change blob container permission to public access
+		private void setContainerPermissions(CloudBlobContainer container)
+		{
+			//NOTE: Anyone on the Internet can see blobs in a public container,
+			//but you can modify or delete them only if you have the appropriate
+			//account access key or a shared access signature.
+
+			container.SetPermissions(new BlobContainerPermissions
+			{
+				PublicAccess = BlobContainerPublicAccessType.Blob
+			});
+		}
+	}
+}
